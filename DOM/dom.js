3eafab5662fecb88179bd89cdda97c79d9c12b4e
arunkudiyal <arunkudiyal@icloud.com>
2023-07-25 12:06:26 +0530

    DOM Methods and Manipulation

--- DOM/dom.js
+++ DOM/dom.js
@@ -20,7 +20,38 @@
 // const myHeader = document.querySelector('form')
 // console.log(myHeader)
 
-console.log( document.querySelector('.list-group-item') )
+// console.log( document.querySelector('.list-group-item') )
 
 // 5. querySelectorAll
-console.log( document.querySelectorAll('.list-group-item') )
+// console.log( document.querySelectorAll('.list-group-item') )
+
+
+// DOM MANIPULATION METHODS :-
+const heading = document.querySelector('#header-title')
+
+// 1. textContent -> text in b/w of the <> and </> of the elt
+// heading.textContent = 'Welcome to DOM';
+
+// 2. innerHTML -> 
+// document.querySelector('#my-div').innerHTML = "<p>Hello...</p>"
+
+// NOTE :- textContent & innerHTML both works the different way,
+// textContent changes the text b/w <> & </> while innerHTML adds a child to the given element
+
+// CSS with JavaScript (.style)
+const secondHeading = document.querySelector('#second-heading')
+
+// JS Function - setTimeOut()
+// Syntax -> setTimeOut( callfn, timeoutDelay (in ms) )
+setTimeout( () => {
+    secondHeading.style.backgroundColor = 'purple'
+    secondHeading.style.color = 'white'
+    secondHeading.style.padding = '10px'
+    secondHeading.style.textAlign = 'center'
+}, 3000)
+
+setTimeout(() => {
+    heading.innerHTML = "<h2>Welocme to DOM</h2>"
+}, 5000)
+
+// EVENTS :-
