1c243fbeae2429cdcf97124aa6ce133a31226f37
arunkudiyal <arunkudiyal@icloud.com>
2023-07-25 11:24:11 +0530

    Introduction to DOM - Identifier Methods

--- DOM/dom.js
+++ DOM/dom.js
@@ -1,9 +1,26 @@
 // DOM -> Document  Object Model
+// console.log(document.all)
+// const myLi = document.all[19]
+// console.log(myLi)
 
-console.dir(document)
-console.log(document.URL)
-console.log(document.all)
+// IDENTIFIER METHODS :-
+// 1. getElementById()
+// const myHeader = document.getElementById('header-title')
+// console.log(myHeader)
 
-const myHeading = document.all[4];
-document.all[4].textContent = "Welcome to DOM"
-console.log(myHeading)
+// // 2. getElementsByClassName
+// const listItems = document.getElementsByClassName('list-group-item')
+// console.log(listItems)
+
+// // 3. getElementsByTagName
+// const myForms = document.getElementsByTagName('form')
+// console.log(myForms)
+
+// 4. querySelector(id/className/tagName)
+// const myHeader = document.querySelector('form')
+// console.log(myHeader)
+
+console.log( document.querySelector('.list-group-item') )
+
+// 5. querySelectorAll
+console.log( document.querySelectorAll('.list-group-item') )
