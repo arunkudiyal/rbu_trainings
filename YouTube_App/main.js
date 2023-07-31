e2caed05305baf0156097059e1c3ef48631fe7b9
arunkudiyal <arunkudiyal@icloud.com>
2023-07-31 10:11:11 +0530

    Changes

--- YouTube_App/main.js
+++ YouTube_App/main.js
@@ -3,22 +3,26 @@
 
 document.getElementById('search-btn').addEventListener('click', () => {
     const text = document.getElementById('search-text').value
-    const url = `https://www.googleapis.com/youtube/v3/search?key=AIzaSyB3LkS1ru9KjWKLNC7RyrXZeHreSY3hs6M&part=snippet&q=${text}&maxResults=50`
+    console.log(text)
+    const url = `https://www.googleapis.com/youtube/v3/search?key=AIzaSyC_2CwII3K3NSMxEUzr-XizxknlEvr0N5s&part=snippet&q=${text}&maxResults=50`
     const xhr = new XMLHttpRequest()
     xhr.open('GET', url)
     xhr.onreadystatechange = () => {
         if(xhr.readyState == 4 && xhr.status == 200) {
             const response = JSON.parse(xhr.responseText)
+            console.log(response)
             let output = ''
             for(let i=0; i < response.items.length; i++) {
                 output += `
-                    <a href="https://www.youtube.com/watch?v=${response.items[i].id.videoId}" target="_blank">
-                        <div style="width: 23%; margin: 10px;">
-                            <img style="width:100%" src="${response.items[i].snippet.thumbnails.high.url}" />
-                            <h4>${response.items[i].snippet.title}</h4>
-                            <p>${response.items[i].snippet.description}</p>
-                        </div>
-                    </a>
+                    <div style="width: 23%; margin: 10px;">
+                        <a href="https://www.youtube.com/watch?v=${response.items[i].id.videoId}" target="_blank">
+                            <div>
+                                <img style="width:100%" src="${response.items[i].snippet.thumbnails.high.url}" />
+                                <h4>${response.items[i].snippet.title}</h4>
+                                <p>${response.items[i].snippet.description}</p>
+                            </div>
+                        </a>
+                    </div>   
                 `
             }
             document.getElementById('my-div').innerHTML = output
